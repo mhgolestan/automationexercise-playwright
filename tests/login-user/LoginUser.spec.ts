f6736993ddc3143aedee9f326975f163fbfa6497
mhgolestan <golestan1369@gmail.com>
2025-11-27 01:01:20 +0100

    Refactor login and registration tests to enhance step descriptions and improve readability

--- tests/login-user/LoginUser.spec.ts
+++ tests/login-user/LoginUser.spec.ts
@@ -1,53 +1,92 @@
 import { test, expect } from "@fixtures/pages.fixtures";
-import { registerUser } from "@datafactory/register";
 import * as fs from "node:fs";
 import * as path from "node:path";
 
 const authFile = path.join(__dirname, '../../.auth/user01Auth.json');
 
-test.describe("Authentication Suite", () => {
+test.describe("User Login Suite", () => {
     test.describe("Login", () => {
-        test.beforeEach(async ({ page, homePage }) => {
-            await homePage.goto();
-            await homePage.popupConsent();
+        const registeredUserPath = path.join(process.cwd(), '.auth/registeredUser.json');
+        const registeredUser = JSON.parse(fs.readFileSync(registeredUserPath, 'utf-8'));
+
+        test.beforeEach(async ({ homePage, page }) => {
+            await test.step("1. Launch browser and 2. Navigate to home page", async () => {
+                await homePage.goto();
+            });
+
+            await test.step("3. Verify that home page is visible successfully", async () => {
+                await page.waitForLoadState('networkidle');
+                await homePage.popupConsent();
+                await expect(homePage.locatorHomepageHeader).toBeVisible();
+            });
+
+            await test.step("4. Click on 'Signup / Login' button", async () => {
+                await homePage.navigateToSignupLogin();
+            });
+
+            await test.step("5. Verify 'Login to your account' is visible", async () => {
+                await expect(page.getByRole("heading", { name: "Login to your account" })).toBeVisible();
+            });
         });
+
         test.afterEach(async ({ page }) => {
             await page.close();
         });
 
-        test("login valid user", async ({ page, homePage, signupLoginPage }) => {
-            await homePage.navigateToSignupLogin();
-            const registeredUserPath = path.join(process.cwd(), '.auth/registeredUser.json');
-            const registeredUser = JSON.parse(fs.readFileSync(registeredUserPath, 'utf-8'));
+        test("Test Case 2: Login User with correct email and password", async ({ page, homePage, signupLoginPage }) => {
 
-            await signupLoginPage.login(registeredUser.email, registeredUser.password);
-            await expect(page.getByText(`Logged in as`)).toBeVisible();
+            await test.step("6. Enter correct email address and password", async () => {
+                await signupLoginPage.loginForm(registeredUser.email, registeredUser.password);
+            });
+
+            await test.step("7. Click 'Login' button", async () => {
+                await signupLoginPage.submitLogin();
+            });
+
+            await test.step("8. Verify that 'Logged in as username' is visible", async () => {
+                await expect(page.getByText(`Logged in as ${registeredUser.name}`)).toBeVisible();
+            });
         });
 
-        test("login invalid user", async ({ page, homePage, signupLoginPage }) => {
-            await homePage.navigateToSignupLogin();
-            await signupLoginPage.login('wrongEmail@test.test', 'wrongPassword');
-            await expect(page.getByText("Your email or password is incorrect!")).toBeVisible();
+        test("Test Case 3: Login User with incorrect email and password", async ({ page, homePage, signupLoginPage }) => {
+
+            await test.step("6. Enter incorrect email address and password", async () => {
+                await signupLoginPage.loginForm('wrongEmail@test.test', 'wrongPassword');
+            });
+
+            await test.step("7. Click 'Login' button", async () => {
+                await signupLoginPage.submitLogin();
+            });
+
+            await test.step("8. Verify that 'Your email or password is incorrect!' is visible", async () => {
+                await expect(page.getByText("Your email or password is incorrect!")).toBeVisible();
+            });
         });
 
-        test.skip("login user with datafactory", async ({ page }) => {
-            const testEmail = `testuser_${Date.now()}@test.test`;
-            const testPassword = "Test@12345";
-            await registerUser(testEmail, testPassword);
-            console.log(`Registered new user with email: ${testEmail} and password: ${testPassword}`);
-            await expect(page.getByText("Login to your account")).toBeVisible();
-        });
-    });
+        test("Test Case 4: Logout user after login", async ({ page, homePage, signupLoginPage }) => {
 
-    test.describe("Logout", () => {
-        test.use({ storageState: authFile });
+            await test.step("6. Enter correct email address and password", async () => {
+                await signupLoginPage.loginForm(registeredUser.email, registeredUser.password);
+            });
 
-        test("logout user", async ({ page, homePage }) => {
-            await homePage.goto();
-            console.log("Using auth file at:", authFile);
-            await page.getByRole("link", { name: "Logout" }).click();
-            await expect(page.getByText("Login to your account")).toBeVisible();
+            await test.step("7. Click 'Login' button", async () => {
+                await signupLoginPage.submitLogin();
+            });
+
+            await test.step("8. Verify that 'Logged in as username' is visible", async () => {
+                await expect(page.getByText(`Logged in as ${registeredUser.name}`)).toBeVisible();
+            });
+
+            await test.step("9. Click 'Logout' button", async () => {
+                await page.getByRole("link", { name: " Logout" }).click();
+            });
+
+            await test.step("10. Verify that user is navigated to login page", async () => {
+                await expect(page.getByText("Login to your account")).toBeVisible();
+            });
         });
+
+
     });
 });
 
--- tests/register-user/register-user.spec.ts
+++ tests/register-user/register-user.spec.ts
@@ -1,65 +1,93 @@
+import { generateUserData } from "@datafactory/register";
 import { test, expect } from "@fixtures/pages.fixtures";
 import * as fs from "node:fs";
 import * as path from "node:path";
 
 test.describe("Register User Suite ", () => {
+
   test.beforeEach(async ({ page, homePage }) => {
-    await homePage.goto();
-    await homePage.popupConsent();
-    await homePage.navigateToSignupLogin();
+    await test.step("1. Launch browser and 2. Navigate to home page", async () => {
+      await homePage.goto();
+    });
+
+    await test.step("3. Verify that home page is visible successfully", async () => {
+      await page.waitForLoadState('networkidle');
+      await homePage.popupConsent();
+      await expect(homePage.locatorHomepageHeader).toBeVisible();
+    });
+
+    await test.step("4. Click on 'Signup / Login' button", async () => {
+      await homePage.navigateToSignupLogin();
+    });
+
+    await test.step("5. Verify 'New User Signup!' is visible", async () => {
+      await expect(page.getByRole("heading", { name: "New User Signup!" })).toBeVisible();
+    });
+
   });
+
   test.afterEach(async ({ page }) => {
     await page.close();
   });
 
-  test("Register New User", async ({ page, signupLoginPage, signupFormPage }) => {
+  test("Test Case 1: Register User", async ({ page, signupLoginPage, signupFormPage }) => {
+    const userData = generateUserData();
 
-    await test.step("Navigate to signup page", async () => {
-      await expect(
-        page.getByRole("heading", { name: "New User Signup!" })
-      ).toBeVisible();
+    await test.step("6. Enter name and email address", async () => {
+      signupLoginPage.signupForm(userData.name, userData.email);
     });
 
-    await test.step("Perform signup", async () => {
-      signupLoginPage.signup(`test_user${Date.now()}`, `test${Date.now()}@test.test`);
+    await test.step("7. Click 'Signup' button", async () => {
+      await signupLoginPage.submitSignup();
+    });
 
+    await test.step("8. Verify that 'Enter Account Information' is visible", async () => {
       await expect(page.getByText("Enter Account Information")).toBeVisible();
     });
 
-    await test.step("Fill signup form", async () => {
-      await signupFormPage.fillSignupForm();
+    await test.step("9, 10, 11 and 12. Fill signup form", async () => {
+      await signupFormPage.fillSignupForm(userData);
     });
 
-    await test.step("Verify account creation and login", async () => {
+    await test.step("13. Click 'Create Account' button", async () => {
+      await signupFormPage.submitSignupForm();
+    });
+
+    await test.step("14. Verify that 'Account Created!' is visible", async () => {
       await expect(page.getByText("Account Created!")).toBeVisible();
+    });
+    await test.step("15. Click 'Continue' button", async () => {
       await page.getByRole("link", { name: "Continue" }).click();
-      await expect(page.getByText("Logged in as test_user")).toBeVisible();
+    });
+    
+    await test.step("16. Verify that 'Logged in as username' is visible", async () => {
+      await expect(page.getByText(`Logged in as ${userData.name}`)).toBeVisible();
     });
 
-    await test.step("Delete account", async () => {
+    await test.step("17. Click 'Delete Account' button", async () => {
       await page.getByRole("link", { name: " Delete Account" }).click();
+    });
+
+    await test.step("18. Verify that 'ACCOUNT DELETED!' is visible and click 'Continue' button", async () => {
       await expect(page.getByText("Account Deleted!")).toBeVisible();
     });
   });
 
 
-  test("signup with already registered email", async ({ page, signupLoginPage }) => {
-    await test.step("Click on 'Signup / Login' button", async () => {
-      await page.getByRole("link", { name: " Signup / Login" }).click();
-    });
-
-    await test.step("Verify 'New User Signup!' is visible", async () => {
-      await expect(page.getByRole("heading", { name: "New User Signup!" })).toBeVisible();
-    });
+  test("Test Case 5: Register User with existing email", async ({ page, signupLoginPage }) => {
 
     const registeredUserPath = path.join(process.cwd(), '.auth/registeredUser.json');
-    const registeredUser = JSON.parse(fs.readFileSync(registeredUserPath, 'utf-8'));
+    const { name, email } = JSON.parse(fs.readFileSync(registeredUserPath, 'utf-8'));
 
-    await test.step("Attempt signup with existing email", async () => {
-    await signupLoginPage.signup(registeredUser.username, registeredUser.email);
+    await test.step("6. Enter name and already registered email address", async () => {
+      await signupLoginPage.signupForm(name, email);
     });
-    
-    await test.step("Verify error 'Email Address already exist!' is visible", async () => {
+
+    await test.step("7. Click 'Signup' button", async () => {
+      await signupLoginPage.submitSignup();
+    });
+
+    await test.step("8. Verify error 'Email Address already exist!' is visible", async () => {
       await expect(page.getByText('Email Address already exist!')).toBeVisible();
     });
   });
